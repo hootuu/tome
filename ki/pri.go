6e7deb255d80ed256961a921ab4b4162de0726a5
Ganser <ganser.uu@gmail.com>
2023-10-25 16:01:22 +0800

    add ki.Sign and ki.VerifySign

--- ki/pri.go
+++ ki/pri.go
@@ -1,7 +1,32 @@
 package ki
 
+import (
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/hootuu/utils/errors"
+	"github.com/hootuu/utils/logger"
+	"go.uber.org/zap"
+)
+
 type PRI string
 
 func (pri PRI) S() string {
 	return string(pri)
+}
+
+func (pri PRI) Sign(message string) (string, *errors.Error) {
+	privateKey, nErr := crypto.HexToECDSA(pri.S()[2:])
+	if nErr != nil {
+		logger.Logger.Error("crypto.HexToECDSA(pri.S())", zap.Error(nErr))
+		return "", errors.Sys("crypto.HexToECDSA error: " + nErr.Error())
+	}
+
+	msgBytes := []byte(message)
+	hash := crypto.Keccak256Hash(msgBytes)
+	signStr, nErr := crypto.Sign(hash.Bytes(), privateKey)
+	if nErr != nil {
+		logger.Logger.Error("crypto.Sign(hash.Bytes(), privateKey)", zap.Error(nErr))
+		return "", errors.Sys("crypto.Sign error: " + nErr.Error())
+	}
+	return hexutil.Encode(signStr), nil
 }
--- ki/pub.go
+++ ki/pub.go
@@ -1,3 +1,33 @@
 package ki
 
+import (
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/hootuu/utils/errors"
+	"github.com/hootuu/utils/logger"
+	"go.uber.org/zap"
+)
+
 type PUB string
+
+func (pub PUB) S() string {
+	return string(pub)
+}
+
+func (pub PUB) VerifySignature(message string, signStr string) (bool, *errors.Error) {
+	publicKeyBytes, nErr := hexutil.Decode(pub.S())
+	if nErr != nil {
+		logger.Logger.Error("hex.DecodeString(pub.S()) failed", zap.Error(nErr))
+		return false, errors.Sys("hex.DecodeString failed: " + nErr.Error())
+	}
+	msgBytes := []byte(message)
+	signBytes, nErr := hexutil.Decode(signStr)
+	if nErr != nil {
+		logger.Logger.Error("hexutil.Decode(signStr) failed", zap.Error(nErr))
+		return false, errors.Sys("hexutil.Decode(signStr) failed: " + nErr.Error())
+	}
+
+	hash := crypto.Keccak256Hash(msgBytes)
+	isValid := crypto.VerifySignature(publicKeyBytes, hash.Bytes(), signBytes[:64])
+	return isValid, nil
+}
